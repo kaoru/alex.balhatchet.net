018ee5265aebd8f4c377d83494a5dfec74ab0250
Alex Balhatchet <kaoru@slackwise.net>
2010-12-08 23:03:13 +0000

    moved lightbox images into their own directory

--- js/photos.js
+++ js/photos.js
@@ -1,8 +1,13 @@
 $(function() {
     $('.gallery').each(function() {
         $(this).children('a').lightBox({
-            txtImage:'Photo',
-            txtOf:'/'
+            txtImage:       'Photo',
+            txtOf:          '/',
+            imageLoading:   'images/lightbox/lightbox-ico-loading.gif',
+            imageBtnClose:  'images/lightbox/lightbox-btn-close.gif',
+            imageBtnPrev:   'images/lightbox/lightbox-btn-prev.gif',
+            imageBtnNext:   'images/lightbox/lightbox-btn-next.gif',
+            imageBlank:     'images/lightbox/lightbox-blank.gif'
         });
     });
 });
